544b3c48ad82dbd45b506b5cd2b0de7b44f90186
Aditya312030 <adityaambadgatti2@gmail.com>
2025-05-27 11:06:00 +0530

    BankCustomer

---
+++ BankCustomer.java
@@ -0,0 +1,32 @@
+package Programs;
+
+// BankCustomer.java
+public class BankCustomer {
+    // Attributes
+    String cust_name;
+    int cust_id;
+    String cust_address;
+
+    // Constructor to initialize values
+    public BankCustomer(String name, int id, String address) {
+        cust_name = name;
+        cust_id = id;
+        cust_address = address;
+    }
+
+    // Method to display customer details
+    public void displayCustomerDetails() {
+        System.out.println("Customer ID: " + cust_id);
+        System.out.println("Customer Name: " + cust_name);
+        System.out.println("Customer Address: " + cust_address);
+    }
+
+    // Main method to test the class
+    public static void main(String[] args) {
+        // Creating a customer object
+        BankCustomer customer1 = new BankCustomer("Aditya", 3108, "123 Main Street, Yashwanthpur");
+
+        // Displaying customer details
+        customer1.displayCustomerDetails();
+    }
+}
